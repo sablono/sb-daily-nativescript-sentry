04be041dcce964d4a2fb7ca6f3788a36f5a82e77
Sebastian Cordes <sebastian.cordes@finanzritter.com>
2020-04-07 17:18:08 +0200

    Update dependencies

--- src/angular/app.module.d.ts
+++ src/angular/app.module.d.ts
@@ -1,10 +1,13 @@
 import { InjectionToken, ModuleWithProviders } from '@angular/core';
 import { SentryErrorHandler } from './error.handler';
+import * as i0 from "@angular/core";
 export interface SentryConfig {
-  dsn: string;
+    dsn: string;
 }
 export declare const SentryService: InjectionToken<SentryConfig>;
 export declare class SentryModule {
-  static forRoot(config: SentryConfig): ModuleWithProviders;
+    static forRoot(config: SentryConfig): i0.ModuleWithProviders<SentryModule>;
+    static ɵmod: i0.ɵɵNgModuleDefWithMeta<SentryModule, never, never, never>;
+    static ɵinj: i0.ɵɵInjectorDef<SentryModule>;
 }
 export declare function provideSentryServiceOptions(config: SentryConfig): SentryErrorHandler;
--- src/angular/error.handler.d.ts
+++ src/angular/error.handler.d.ts
@@ -1,6 +1,12 @@
 import { ErrorHandler } from '@angular/core';
+import * as i0 from "@angular/core";
 export declare class SentryErrorHandler extends ErrorHandler {
-  private config;
-  constructor(config: any);
-  handleError(err: any): void;
+    private config;
+    private _discardUncaughtJsExceptions;
+    constructor(config: any);
+    handleError(err: any): void;
+    private _setDSN;
+    private _setDiscardUncaughtJsExceptions;
+    static ɵfac: i0.ɵɵFactoryDef<SentryErrorHandler, never>;
+    static ɵprov: i0.ɵɵInjectableDef<SentryErrorHandler>;
 }

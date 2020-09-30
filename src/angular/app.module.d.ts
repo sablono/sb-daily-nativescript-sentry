338cd1a4ba7b250b3cec48425bff0d14ac8dd887
Jeremy PELE <jeremy.pele@gmail.com>
2020-09-30 14:57:38 +0000

    Update declarations

--- src/angular/app.module.d.ts
+++ src/angular/app.module.d.ts
@@ -1,10 +1,11 @@
 import { InjectionToken, ModuleWithProviders } from '@angular/core';
 import { SentryErrorHandler } from './error.handler';
 export interface SentryConfig {
-  dsn: string;
+    dsn: string;
+    discardUncaughtJsExceptions?: boolean;
 }
 export declare const SentryService: InjectionToken<SentryConfig>;
 export declare class SentryModule {
-  static forRoot(config: SentryConfig): ModuleWithProviders<SentryModule>;
+    static forRoot(config: SentryConfig): ModuleWithProviders<SentryModule>;
 }
 export declare function provideSentryServiceOptions(config: SentryConfig): SentryErrorHandler;
--- src/angular/error.handler.d.ts
+++ src/angular/error.handler.d.ts
@@ -1,9 +1,7 @@
 import { ErrorHandler } from '@angular/core';
-export declare class SentryErrorHandler extends ErrorHandler {
-  private config;
-  private _discardUncaughtJsExceptions;
-  constructor(config: any);
-  handleError(err: any): void;
-  private _setDSN;
-  private _setDiscardUncaughtJsExceptions;
+export declare class SentryErrorHandler implements ErrorHandler {
+    private _config;
+    constructor(_config: any);
+    handleError(err: any): void;
+    private _setDSN;
 }

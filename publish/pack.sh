bc134a841685c5cf0715386ca901c6136738acc3
Jeremy PELE <jeremy.pele@gmail.com>
2020-09-30 14:26:02 +0000

    Pack angular

--- publish/pack.sh
+++ publish/pack.sh
@@ -29,6 +29,7 @@ pack() {
     echo 'Building /src...'
     cd "$TO_SOURCE_DIR"
     node_modules/.bin/tsc
+    node_modules/.bin/ngc -skipLibCheck --project tsconfig.json
     cd ..
 
     echo 'Creating package...'
